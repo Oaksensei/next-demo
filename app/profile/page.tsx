9fa97a034ed478ece43d3fefd6f869acc23f1cf9
Oaksensei <pongpipat1223@gmail.com>
2025-09-13 09:45:03 +0700

    abc

---
+++ app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+  return (
+    <section>
+      <h1>Profile</h1>
+      <p>Pongpipat Muangdung 66107251</p>
+      <p>Pisit Rodkleang 66108168</p>
+    </section>
+  );
+}
